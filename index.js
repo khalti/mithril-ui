183f2b1e921a3ca5be38ecc284f5d2dae5b2a873
Ludbek <sth.srn@gmail.com>
2016-01-14 12:07:32 +0545

    hohooo

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const m = require('mithril');
+const _ = require('lodash');
+
+module.exports = {
+  controller: function () {
+  },
+  view: function () {
+    return m('h1', 'Hello This worked bro.');
+  }
+};
---
+++ readme.md
@@ -0,0 +1,2 @@
+Self Executing Package
+  $ jspm bundle-sfx index.js - mithril - lodash - velocity dist/selection.js --globals "{mithril:'m',lodash:'_'}" --global-name Selection

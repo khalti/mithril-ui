10ff616238e53bc8fc7545bfaa83ba4fbb55ab6c
ludbek <sth.srn@gmail.com>
2016-03-10 12:01:02 +0545

    updated classes at Input

--- spec/components/input-spec.js
+++ spec/components/input-spec.js
@@ -22,7 +22,7 @@ describe("Input", function () {
   })
 
   it("it sets root's class", function () {
-    expect(aView.attrs.class).toEqual(".ui.right.labeled.input")
+    expect(aView.attrs.class).toEqual("ui right labeled input")
   })
 
   it("passes rest of the attributes to input element", function () {
@@ -43,37 +43,37 @@ describe("Input", function () {
 
     it("returns '.ui.input' if there is nothing to prepend or append", function () {
       var attrs = {}
-      expect(ctrl.getClass(attrs)).toEqual(".ui.input")
+      expect(ctrl.getClass(attrs)).toEqual("ui input")
     })
 
     it("returns '.ui.icon.input' if an icon is being appended", function () {
       var attrs = {append: m("i")}
-      expect(ctrl.getClass(attrs)).toEqual(".ui.icon.input")
+      expect(ctrl.getClass(attrs)).toEqual("ui icon input")
     })
 
     it("returns '.ui.left.icon.input' if an icon is being prepended", function () {
       var attrs = {prepend: m("i")}
-      expect(ctrl.getClass(attrs)).toEqual(".ui.left.icon.input")
+      expect(ctrl.getClass(attrs)).toEqual("ui left icon input")
     })
 
     it("returns '.ui.labeled.input' if a label is being prepended", function () {
       var attrs = {prepend: m("div")}
-      expect(ctrl.getClass(attrs)).toEqual(".ui.labeled.input")
+      expect(ctrl.getClass(attrs)).toEqual("ui labeled input")
     })
 
     it("returns '.ui.right.labeled.input' if a label is being appended", function () {
       var attrs = {append: m("div")}
-      expect(ctrl.getClass(attrs)).toEqual(".ui.right.labeled.input")
+      expect(ctrl.getClass(attrs)).toEqual("ui right labeled input")
     })
 
     it("returns '.ui.labeled.icon.input' if a label is being prepended and icon is being appended", function () {
       var attrs = {prepend: m("div"), append: m("i")}
-      expect(ctrl.getClass(attrs)).toEqual(".ui.labeled.icon.input")
+      expect(ctrl.getClass(attrs)).toEqual("ui labeled icon input")
     })
 
     it("returns '.ui.left.icon.right.labeled.input' if an icon is being prepended and a label is being appended", function () {
       var attrs = {prepend: m("i"), append: m("div")}
-      expect(ctrl.getClass(attrs)).toEqual(".ui.left.icon.right.labeled.input")
+      expect(ctrl.getClass(attrs)).toEqual("ui left icon right labeled input")
     })
   })
 });

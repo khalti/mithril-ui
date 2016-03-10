551876035b6fc68e968f788010b17a8f891840c8
ludbek <sth.srn@gmail.com>
2016-03-10 08:19:44 +0545

    Working on new formModel api

--- utils/form-model.js
+++ utils/form-model.js
@@ -7,33 +7,34 @@ function prop(model, field, defaultValue) {
 
   var aclosure = function (value) {
     if(arguments.length === 0)  return state;
-
-    // for equality
-    if (model._config[field].equality) {
-      var equalAgainst = model._config[field].equality;
-      var values = {}
-      values[equalAgainst] = model[equalAgainst]()
-      values[field] = value
-
-      var constrains = {}
-      constrains[field] = _.omit(model._config[field], ['default'])
-
-      var errors = validate(values, constrains);
-      if (!model.errors) model.errors = {} // formModel.is_valid() sets it undefined
-      aclosure.errors = model.errors[field] = errors? errors[field]: undefined;
-      }
-    else {
-      if(!model.errors) model.errors = {}
-      aclosure.errors = model.errors[field] = validate.single(
-        value,
-        _.omit(model._config[field], ['default']));
-      }
     state = value;
     };
 
   aclosure.is_dirty = function() {
     return initialState !== state;
     };
+
+  aclosure.is_valid = function (attach_errors) {
+    var errors;
+    var constrains = {}
+    constrains[field] = _.omit(model._config[field], ['default'])
+    var values = {}
+    values[field] = aclosure()
+
+    // for equality
+    if (model._config[field].equality) {
+      var equalAgainst = model._config[field].equality;
+      values[equalAgainst] = model[equalAgainst]()
+      }
+
+    errors = validate(values, constrains);
+    if(attach_errors !== false) {
+      if (!model.errors) model.errors = {} // formModel.is_valid() sets it undefined
+      aclosure.errors = model.errors[field] = errors? errors[field]: undefined;
+    }
+
+    return errors === undefined
+  }
 
   return aclosure;
   };
@@ -63,10 +64,6 @@ module.exports =  function (config) {
       return _.some(_.keys(this._config), function (akey) {
         return self[akey].is_dirty();
         });
-      },
-
-    validate: function () {
-      this.errors = validate(this.values(), this._config);
       },
 
     values: function () {

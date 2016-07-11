f54c712150a460d6e4fe40da9d51e6d26c071415
ludbek <sth.srn@gmail.com>
2016-07-11 11:51:48 +0545

    removed factory

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = {
   shortcuts: require("./shortcuts.js"),
-  factory: require("./helpers/factory.js"),
+  component: require("./helpers/component.js"),
   shortcutFactory: require("./helpers/shortcutFactory.js"),
   input: require('./components/input.js'),
   field: require('./components/field.js'),

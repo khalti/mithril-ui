01a73a6eefe248fe9b089d483d15d48f97900ed4
ludbek <sth.srn@gmail.com>
2016-08-05 11:51:36 +0545

    bug fixes

--- src/components/button.js
+++ src/components/button.js
@@ -3,6 +3,9 @@ import component from "mithril-componentx";
 
 export const button = component({
   base: base,
+	getDefaultAttrs (attrs) {
+		return {dom: {tagName: "form"}};
+	},
 	getClassList (attrs) {
 		return ["ui",
 						"button"];
--- src/components/form.js
+++ src/components/form.js
@@ -5,6 +5,9 @@ import m from "mithril";
 
 export const form = component({
 	base: base,
+	getDefaultAttrs (attrs) {
+		return {dom: {tagName: "form"}};
+	},
 	getClassList (attrs) {
 		return ["ui",
 						"form"];

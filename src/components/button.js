73642939899df3a311ab9d76ceee4002400a4a8e
ludbek <sth.srn@gmail.com>
2016-08-08 13:16:22 +0545

    button done

--- src/components/button.js
+++ src/components/button.js
@@ -1,13 +1,46 @@
 import {base} from "./base.js";
 import component from "mithril-componentx";
+import m from "mithril";
+import omit from "lodash/omit";
+import {colorClassMap} from "./../helpers/enums.js";
+
+let floatMap = {
+	right: "right floated",
+	left: "left floated"
+};
 
 export const button = component({
   base: base,
+	attrSchema: {
+		label: {presence: true}
+	},
 	getDefaultAttrs (attrs) {
-		return {dom: {tagName: "form"}};
+		return {dom: {tagName: "button"}};
 	},
 	getClassList (attrs) {
-		return ["ui",
-						"button"];
+		return [
+			"ui",
+			attrs.size,
+			floatMap[attrs.float],
+			colorClassMap[attrs.color],
+			{circular: attrs.circular},
+			{fluid: attrs.fluid},
+			{compact: attrs.compact},
+			{loading: attrs.loading},
+			{disabled: attrs.disabled},
+			{active: attrs.active},
+			{basic: attrs.basic},
+			{inverted: attrs.inverted},
+			{labeled: attrs.icon && attrs.label? true: false},
+			{icon: attrs.icon? true: false},
+			"button"
+		];
+	},
+	view (vnode) {
+		let attrs = vnode.attrs;
+
+		return m(attrs.dom.tagName, omit(attrs.dom, ["tagname"]),
+						 attrs.icon,
+						 attrs.label);
 	}
 });

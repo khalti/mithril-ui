aee48cc2ca0c632bb34574fd3872ab8383888229
ludbek <sth.srn@gmail.com>
2016-09-28 11:14:36 +0545

    sub menu can be right floated

--- src/components/menu/subMenu.js
+++ src/components/menu/subMenu.js
@@ -1,9 +1,16 @@
 import {base} from "./../base.js";
 import component from "mithril-componentx";
+import {required, within} from "validatex";
 
 export const subMenu = component({
 	base: base,
+	attrSchema: {
+		right: [required(false), within([true, false])]
+	},
 	getClassList (attrs) {
-		return ["menu"];
+		return [
+			{right: attrs.right},
+			"menu"
+		];
 	}
 });

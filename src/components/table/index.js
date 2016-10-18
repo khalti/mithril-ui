80b50e8d02bf511ff12a8db9bb482f4c477ec9ea
ludbek <sth.srn@gmail.com>
2016-10-18 16:57:50 +0545

    fixed a bug at getClasList in tbody

---
+++ src/components/table/index.js
@@ -0,0 +1,10 @@
+import {table} from "./table.js";
+import {thead} from "./thead.js";
+import {tbody} from "./tbody.js";
+import {tfoot} from "./tfoot.js";
+import {tr} from "./tr.js";
+import {th} from "./th.js";
+import {td} from "./td.js";
+
+export {table, thead, tbody, tfoot, tr,th , td};
+
--- src/components/table/tbody.js
+++ src/components/table/tbody.js
@@ -8,8 +8,6 @@ export const tbody = component({
 	base: base,
 	attrSchema: {
 	},
-	getClassList (attrs) {
-	},
 	getDefaultAttrs (attrs) {
 		return {root: "tbody"};
 	}

ac4197516a3256e8d647ed74e323127edad0d5e9
ludbek <sth.srn@gmail.com>
2016-11-14 18:13:45 +0545

    pagination updates

--- spec/components/paginationSpec.js
+++ spec/components/paginationSpec.js
@@ -1,6 +1,7 @@
 import {expect} from "chai";
 import _ from "mithril";
 import {pagination} from "./../../src/components/pagination.js";
+import {getVdom} from "./../../spec/utils.js";
 
 
 describe("pagination", () => {
@@ -29,6 +30,13 @@ describe("pagination", () => {
 			onPageChange: () => {}
 		};
 		expect(pagination.view.bind(pagination, attrs)).to.throw(Error);
+	});
+
+	describe(".getClassList", () => {
+		it("includes 'pagination'", () => {
+			let got = pagination.getClassList({});
+			expect(got.indexOf("pagination")).not.to.equal(-1);
+		});
 	});
 
 	describe(".getPages", () => {
@@ -65,5 +73,156 @@ describe("pagination", () => {
 			let expected2 = [8 , 9, 10];
 			expect(got2).to.eql(expected2);
 		});
+	});
+
+	describe(".getFirstPageBtn", () => {
+		let attrs, clicked, clickEvent;
+
+		beforeEach(() => {
+			clicked = false;
+
+			clickEvent = {
+				preventDefault: () => {}
+			};
+
+			attrs = {
+				onPageChange: () => {
+					clicked = true;
+				},
+				pageCount: 10,
+				currentPage: undefined
+			};
+		});
+
+		it("sets icon to 'angle double left'", () => {
+			let vdom = getVdom(pagination.getFirstPageBtn({}));
+
+			let got = vdom.children[0].attrs.class;
+			expect(got).to.contain("angle double left");
+		});
+
+		it("disables the button", () => {
+			attrs.currentPage = 1;
+			let vdom = getVdom(pagination.getFirstPageBtn(attrs));
+
+			let got = vdom.attrs.class;
+
+			expect(got).to.contain("disabled");
+		});
+
+		it("sets 'href' to '#'", () => {
+			attrs.currentPage = 2;
+			let vdom = getVdom(pagination.getFirstPageBtn(attrs));
+
+			let got = vdom.attrs.href;
+
+			expect(got).to.equal("#");
+		});
+
+		it("sets 'href' to ''", () => {
+			attrs.currentPage = 1;
+			let vdom = getVdom(pagination.getFirstPageBtn(attrs));
+
+			let got = vdom.attrs.href;
+
+			expect(got).to.equal("");
+		});
+
+		it("sets current page to 1 on click.", () => {
+			let vdom = getVdom(pagination.getFirstPageBtn(attrs));
+			vdom.attrs.onclick(clickEvent);
+
+			expect(clicked).to.equal(true);
+		});
+	});
+
+	describe(".getLastPageBtn", () => {
+		let attrs, clicked, clickEvent;
+
+		beforeEach(() => {
+			clicked = false;
+
+			clickEvent = {
+				preventDefault: () => {}
+			};
+
+			attrs = {
+				onPageChange: () => {
+					clicked = true;
+				},
+				pageCount: 10,
+				currentPage: undefined
+			};
+		});
+
+		it("sets icon to 'angle double right'", () => {
+			let vdom = getVdom(pagination.getLastPageBtn({}));
+
+			let got = vdom.children[0].attrs.class;
+			expect(got).to.contain("angle double right");
+		});
+
+		it("disables the button", () => {
+			attrs.currentPage = 10;
+			let vdom = getVdom(pagination.getLastPageBtn(attrs));
+
+			let got = vdom.attrs.class;
+
+			expect(got).to.contain("disabled");
+		});
+
+		it("sets 'href' to '#'", () => {
+			attrs.currentPage = 9;
+			let vdom = getVdom(pagination.getLastPageBtn(attrs));
+
+			let got = vdom.attrs.href;
+
+			expect(got).to.equal("#");
+		});
+
+		it("sets 'href' to ''", () => {
+			attrs.currentPage = 10;
+			let vdom = getVdom(pagination.getLastPageBtn(attrs));
+
+			let got = vdom.attrs.href;
+
+			expect(got).to.equal("");
+		});
+
+		it("sets current page to last page on click.", () => {
+			let vdom = getVdom(pagination.getLastPageBtn(attrs));
+			vdom.attrs.onclick(clickEvent);
+
+			expect(clicked).to.equal(true);
+		});
+	});
+
+	describe(".showLeft3dotsBtn", () => {
+		it("returns true if visible pages do not contain the 1st page.", () => {
+			expect(pagination.showLeft3dotsBtn(10, 4)).to.equal(true);
+		});
+
+		it("returns false if visible pages contain the 1st page.", () => {
+			expect(pagination.showLeft3dotsBtn(10, 3)).to.equal(false);
+		});
+	});
+
+	describe(".showRight3dotsBtn", () => {
+		it("returns true if visible pages do not contain the last page.", () => {
+			expect(pagination.showRight3dotsBtn(10, 7)).to.equal(true);
+		});
+
+		it("returns false if visible page contains the last page.", () => {
+			expect(pagination.showRight3dotsBtn(10, 8)).to.equal(false);
+		});
+	});
+
+	describe.only(".getPageBtn", () => {
+		it("displays page number");
+
+		it("disables button if current page and button page are same.");
+		it("sets 'href' to '#'");
+		it("sets 'href' to ''");
+		it("calls 'onPageChange' on click.");
 	});
 });
--- src/components/pagination.js
+++ src/components/pagination.js
@@ -2,6 +2,7 @@ import component from "mithril-componentx";
 import {menu, item} from "./menu";
 import _ from "mithril";
 import {required, isNumber, isFunction} from "validatex";
+import {icon} from "./icon.js";
 
 
 export const pagination = component({
@@ -35,29 +36,41 @@ export const pagination = component({
 	},
 
 	getFirstPageBtn ({onPageChange, pageCount, currentPage}) {
-		return _(item, { "data-page": 1,
-											disabled: currentPage == 1,
-											href: "#",
-											onclick: this.getClickHandler(onPageChange, 1, currentPage) },
+		let isCurrentPage = currentPage === 1;
+
+		return _(item, {disabled: isCurrentPage,
+										href: isCurrentPage? "" : "#",
+										onclick: this.getClickHandler(onPageChange, 1, currentPage) },
 							_(icon, {class: "angle double left large icon"}));
 	},
 
-
 	getLastPageBtn ({onPageChange, pageCount, currentPage}) {
-		return _(item, { "data-page": pageCount,
-											href: "#",
-											disabled: pageCount == currentPage,
-											onclick: this.getClickHandler(onPageChange, pageCount, currentPage) },
+		let isCurrentPage = currentPage === pageCount;
+
+		return _(item, {href: isCurrentPage? "": "#",
+										disabled: isCurrentPage,
+										onclick: this.getClickHandler(onPageChange, pageCount, currentPage) },
 							_(icon, {class: "angle double right large icon"}));
 	},
 
+	showLeft3dotsBtn(pageCount, currentPage) {
+		return this.getVisiblePages(pageCount, currentPage).indexOf(1) === -1;
+	},
+
+	showRight3dotsBtn (pageCount, currentPage) {
+		return this.getVisiblePages(pageCount, currentPage).indexOf(pageCount) === -1;
+	},
+
 	get3dotsBtn () {
-		return _(item, {disabled: true, href: "#", onclick: (e) => {e.preventDefault();}},
+		return _(item, {disabled: true},
 						_(icon, {class: "ellipsis horizontal large icon"}));
 	},
 
-	getPageBtn ({onPageChange, pageNumber, currentPage}) {
-		return _(item, {active: pageNumber === currentPage,
+	getPageBtn (onPageChange, pageNumber, currentPage) {
+		let isCurrentPage = pageNumber === currentPage;
+
+		return _(item, {active: isCurrentPage,
+										href: isCurrentPage? "": "#",
 										onclick: this.getClickHandler(onPageChange, pageNumber, currentPage)},
 							page)
 	},
@@ -70,8 +83,52 @@ export const pagination = component({
 		};
 	},
 
-	view ({attrs, children, state}) {
+	getPagesBtns ({onPageChange, pageCount, currentPage}) {
+		return this.getPages(pageCount).map((page) => {
+			return this.getPageBtn(onPageChange, page, currentPage);
+		});
+	},
+
+	getNextPageBtn ({onPageChange, pageCount, currentPage}) {
+		let nextPage = currentPage + 1;
+		let disabled = nextPage === pageCount;
+
+		return _(item, {href: disabled? "": "#",
+										disabled: disabled,
+										onclick: this.getClickHandler(onPageChange, nextPage, currentPage)},
+						_(icon, {class: "angle right large icon"}));
+	},
+
+	getPreviousPageBtn ({onPageChange, pageCount, currentPage}) {
+		let previousPage = currentPage - 1;
+		let disabled = nextPage === pageCount;
+
+		return _(item, {href: disabled? "": "#",
+										disabled: disabled,
+										onclick: this.getClickHandler(onPageChange, previousPage, currentPage)},
+						_(icon, {class: "angle left large icon"}));
+	},
+
+	getItems (attrs) {
 		let {onPageChange, pageCount, currentPage} = attrs;
-		return _("div", attrs.rootAttrs, );
+		let items = this.getPagesBtn(attrs);
+
+		// add ... at either side of visible pages
+		this.showLeft3dotsBtn(pageCount, currentPage)? items.unshift(this.get3dotsBtn()): null;
+		this.showRight3dotsBtn(pageCount, currentPage)? items.push(this.get3dotsBtn()): null;
+
+		// add < and >
+		items.unshift(this.getPreviousPageBtn(attrs));
+		items.push(this.getNextPageBtn(attrs));
+
+		// add << and >>
+		items.unshift(this.getFirstPageBtn(attrs));
+		items.push(this.getLastPageBtn(attrs));
+
+		return items;
+	},
+
+	view ({attrs, children, state}) {
+		return _("div", attrs.rootAttrs, this.getItems(attrs));
 	}
 });

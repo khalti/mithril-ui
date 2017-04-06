ba60d9d5bff585aaee3d4db06f73968115da97d6
Raj Subit <rajsubit@gmail.com>
2017-04-06 17:34:07 +0545

    radio button extended from field

---
+++ src/components/form/radio.js
@@ -0,0 +1,37 @@
+import o from "mithril";
+import {required} from "validatex";
+import {Field, field} from "./../ui";
+
+
+class Radio extends Field {
+	attrSchema =
+		{ model: required(true)
+		, label: required(true) }
+
+	setValue (attrs) {
+		attrs.model.setAndValidate(attrs.value);
+	}
+
+	getLabelAppend (attrs) {
+		if(attrs.help && !attrs.model.errors()) {
+			return o('label.help', attrs.help);
+		}
+		else if(attrs.model.error() && !attrs.hideError) {
+			return o('label.error', attrs.model.error());
+		}
+		return null;
+	}
+
+  view (vnode) {
+		let attrs = vnode.attrs;
+		attrs.rootAttrs.onclick = this.setValue.bind(this, attrs);
+
+    return o('div', attrs.rootAttrs,
+             o(".ui.radio.checkbox", {className: attrs.model()===attrs.value? "checked": ""},
+               o("input[type=radio][tabindex=0]", {name: attrs.name, value: attrs.model()}),
+               o("label", attrs.label)),
+             this.getLabelAppend(attrs));
+  }
+}
+
+export const radio = new Radio();

bdc7e49ea2c3b5a3516cb3e0f4f12dd1e2efcdda
Raj Subit <rajsubit@gmail.com>
2017-06-23 07:19:04 +0545

    fluid default set to true

--- src/components/form/select.js
+++ src/components/form/select.js
@@ -40,7 +40,7 @@ export class Select extends Field {
 			, placeholder: attrs.placeholder
 			, name: attrs.name
 			, search: attrs.search
-			, fluid: attrs.fluid ? attrs.fluid : true
+			, fluid: attrs.fluid === false ? false : true
 			, options: attrs.options };
 
 		// if (attrs.name) {

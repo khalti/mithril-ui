c46a714a6df778737b6e22cf141130fa06d4ae44
Raj Subit <rajsubit@gmail.com>
2017-08-01 18:37:12 +0545

    multiple values saved in checkbox as list

--- src/components/form/checkbox.js
+++ src/components/form/checkbox.js
@@ -1,6 +1,6 @@
 import _ from "mithril";
 import {Field} from "./field.js";
-import {required, within} from "validatex";
+import {required, within, validate, isArray} from "validatex";
 
 const TYPES = ["slider", "toggle"];
 
@@ -8,11 +8,52 @@ export class Checkbox extends Field {
 	attrSchema = {
 		model: required(true),
 		label: required(true),
+		value: required(true),
+		multiple: required(false),
 		type: [required(false), within(TYPES)]
 	}
 
-	toggleState (attrs) {
-		attrs.model.setAndValidate(!attrs.model());
+	validateAttrs (attrs) {
+		let errors = validate(attrs, this.attrSchema) || {};
+		if (attrs.multiple && attrs.model()) {
+			let err = validate(attrs.model(), isArray());
+			if (err) {
+				errors["default"] = err;
+			}
+		}
+		if (Object.keys(errors).length) {
+			throw Error(JSON.stringify(errors));
+		}
+	}
+
+	getDefaultAttrs (vnode) {
+		let attrs = super.getDefaultAttrs(vnode);
+		attrs.multiple = false;
+		return attrs;
+	}
+
+	setValue (attrs) {
+		if (!attrs.multiple) {
+			attrs.model.setAndValidate(!attrs.model());
+		}
+		else {
+			let val = attrs.model() || [];
+			let index = val.indexOf(attrs.value);
+			if (index > -1) {
+				val.splice(index, 1);
+			}
+			else {
+				val.push(attrs.value);
+			}
+			attrs.model.setAndValidate(val);
+		}
+	}
+
+	shouldCheck(attrs) {
+		if (!attrs.multiple) {
+			return attrs.model() === attrs.value;
+		}
+		return (attrs.model() || []).indexOf(attrs.value) > -1;
 	}
 
 	getLabelAppend (attrs) {
@@ -26,15 +67,15 @@ export class Checkbox extends Field {
 	}
 
   view ({attrs, children, state}) {
-		attrs.rootAttrs.onclick = this.toggleState.bind(this, attrs);
+		attrs.rootAttrs.onclick = this.setValue.bind(this, attrs);
 
 		let checkboxClasses = [];
-		attrs.model() && checkboxClasses.push("checked");
+		this.shouldCheck(attrs) && checkboxClasses.push("checked");
 		attrs.type && checkboxClasses.push(attrs.type);
 
     return _('div', attrs.rootAttrs,
              _(".ui.checkbox", {className: checkboxClasses.join(" ")},
-               _("input.hidden[type=checkbox][tabindex=0]", {checked: attrs.model()}),
+               _("input.hidden[type=checkbox][tabindex=0]", {checked: this.shouldCheck(attrs)}),
                _("label", attrs.label)),
              this.getLabelAppend(attrs));
   }
